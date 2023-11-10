60dfefac122b865f6aa377b681ee281902c166be
rithinprem <rithinengineeringmy@gmail.com>
2023-11-10 06:35:29 +0530

    second commit

--- app.py
+++ app.py
@@ -12,16 +12,21 @@ def homepage():
 @app.route('/weatherapp',methods =['POST','GET'])
 def get_weatherdata():
     url = "https://api.openweathermap.org/data/2.5/weather"
+   
     params ={ 'q':request.form.get("city"),
-         'appid': request.form.get("appid"),
-          'units':request.form.get("units")}
+             'appid': "5174c48a3535ca0b041ba725368a7722",
+              'units':"metric"}
+
 
     response = requests.get(url,params)
     data = response.json()
     city= data['name']
 
 
-    return f"data:{data}, {city}"
+    return render_template("output.html",weather_data=data)
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0",port=5002)
+    app.run(host="0.0.0.0",port=5005)
+
+
+
