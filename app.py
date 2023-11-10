c58b797855c2815f51ec147f8b1c094fc169a920
rithinprem <yorithinengineeringmy@gmail.com>
2023-11-11 02:07:30 +0530

    uptimerobot

--- app.py
+++ app.py
@@ -5,40 +5,6 @@ import time
 
 app = Flask(__name__)
 
-# Variable to indicate whether the application is active
-app_active = True
-
-# Function to reset the app_active variable after a certain time of inactivity
-def reset_activity():
-    global app_active
-    while True:
-        time.sleep(840)  # 14 minutes
-        app_active = False
-
-# Function to automatically ping the /ping endpoint at regular intervals
-def automatic_ping():
-    while True:
-        # Use the test client to simulate a request to the /ping endpoint
-        with app.test_client() as client:
-            client.get('/ping')
-
-        time.sleep(60)  # Adjust the interval based on your needs (e.g., every 10 minutes)
-
-# Start the background thread to reset activity
-activity_reset_thread = threading.Thread(target=reset_activity)
-activity_reset_thread.start()
-
-# Start the background thread for automatic pinging
-ping_thread = threading.Thread(target=automatic_ping)
-ping_thread.start()
-
-# Route for pinging to keep the application active
-@app.route('/ping')
-def ping():
-    global app_active
-    app_active = True
-    return jsonify(status='ok')
-
 @app.route('/')
 def homepage():
     return render_template("index.html")
@@ -50,16 +16,6 @@ def get_weatherdata():
     response = requests.get(url, params)
     data = response.json()
     return render_template("output.html", weather_data=data)
-
-# Before each request, check if the app should be considered active
-@app.before_request
-def before_request():
-    global app_active
-    if not app_active:
-        # Perform any necessary actions to "wake up" the application
-        # For example, you might make a request to another endpoint or perform initialization
-        # In this example, we'll just set app_active to True
-        app_active = True
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5002)
