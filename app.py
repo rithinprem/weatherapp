f8f7af1b5e53fdbd5f2685638bb80744a156943f
rithinprem <yorithinengineeringmy@gmail.com>
2023-11-11 04:25:18 +0530

    final commit

--- app.py
+++ app.py
@@ -1,17 +1,9 @@
 from flask import Flask, render_template, request, jsonify
 import requests
-import schedule
-import time
 import threading
+import time
 
 app = Flask(__name__)
-
-
-@app.route("/ping")
-def ping():
-    print("pinging..")
-    return jsonify(status="ok")
-
 
 @app.route('/')
 def homepage():
@@ -25,33 +17,5 @@ def get_weatherdata():
     data = response.json()
     return render_template("output.html", weather_data=data)
 
-
-
-def send_request():
-    # Define the URL you want to send the request to
-    url = 'http://localhost:5003/ping'
-
-    # Make the HTTP request using the 'requests' library
-    response = requests.get(url)
-
-    # Access the response content
-    content = response.content
-
-    # Process the content or log it as needed
-    print(content)
-
-def run_flask():
-    app.run(host="0.0.0.0", port=5003, threaded=True)
-
 if __name__ == "__main__":
-    # Start the Flask app in a separate thread
-    flask_thread = threading.Thread(target=run_flask)
-    flask_thread.start()
-
-    # Schedule the job to run every 10 minutes
-    schedule.every(10).minutes.do(send_request)
-
-    # Run the scheduled jobs continuously
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
+    app.run(host="0.0.0.0", port=5003)
